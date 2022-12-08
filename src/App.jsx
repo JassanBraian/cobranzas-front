62bc99750f628299e23d1e0bc673386536f0bbe6
Braian Jassan <braianjassan@gmail.com>
2022-12-08 20:56:29 -0300

    add route carreras

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import ParentContext from './context/ParentContext';
 import Home from './pages/Home';
+import CarrerasPage from './pages/CarrerasPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Router>
           <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/carreras' element={<CarrerasPage />} /> 
           </Routes>
         </Router>
       </ParentContext>
---
+++ src/pages/CarrerasPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CarrerasPage = () => {
+    return (
+        <div>
+            Ubicado en carreras page
+        </div>
+    );
+};
+
+export default CarrerasPage;
